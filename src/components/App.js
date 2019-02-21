0c290a998ac26c1296f2978aa10422dafeb6931e
Nolan Kovacik <noltron000@gmail.com>
2019-02-20 17:12:50 -0800

    🔖 spaces to tabs

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,13 @@ import './App.css';
 import Counter from './Counter'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Counter />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Counter />
+			</div>
+		);
+	}
 }
 
 export default App;
